8d8e647df92874c335f70701f6ffcd32c6aeebe2
Emmanuel <1090777+emmanuelstroem@users.noreply.github.com>
2025-08-07 14:21:03 +0200

    Initial Commit

---
+++ lytterTests/lytterTests.swift
@@ -0,0 +1,16 @@
+//
+//  lytterTests.swift
+//  lytterTests
+//
+//  Created by Emmanuel on 07/08/2025.
+//
+
+import Testing
+
+struct lytterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
